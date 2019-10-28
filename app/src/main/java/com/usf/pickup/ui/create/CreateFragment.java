3d843e103812e16bd0a5f4b43e804654a44a02f3
JT <jtcullen1998@gmail.com>
2019-10-28 00:48:02 -0400

    Get started on create games page functionality

--- app/src/main/java/com/usf/pickup/ui/create/CreateFragment.java
+++ app/src/main/java/com/usf/pickup/ui/create/CreateFragment.java
@@ -1,33 +1,171 @@
 package com.usf.pickup.ui.create;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.usf.pickup.R;
 
-public class CreateFragment extends Fragment {
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
+public class CreateFragment extends Fragment {
     private CreateViewModel createViewModel;
+    private SimpleDateFormat dateformat = new SimpleDateFormat("MMMM dd, yyyy", Locale.US);
+    private SimpleDateFormat timeformat = new SimpleDateFormat("h:m a", Locale.US);
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Places.initialize(getContext(), getString(R.string.places_api_key));
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         createViewModel =
                 ViewModelProviders.of(this).get(CreateViewModel.class);
         View root = inflater.inflate(R.layout.fragment_create, container, false);
-        createViewModel.getText().observe(this, new Observer<String>() {
+
+        final Spinner sportSpinner = root.findViewById(R.id.sport_dropdown);
+        final LinearLayout date = root.findViewById(R.id.dateLayout);
+        final TextView dateTextView = root.findViewById(R.id.dateTextView);
+        final LinearLayout startsAt = root.findViewById(R.id.startsAtLayout);
+        final TextView startsAtTextView = root.findViewById(R.id.startsAtTextView);
+        final LinearLayout endsAt = root.findViewById(R.id.endsAtLayout);
+        final TextView endsAtTextView = root.findViewById(R.id.endsAtTextView);
+        final AutocompleteSupportFragment autocompleteFragment =
+                (AutocompleteSupportFragment) getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
-            public void onChanged(@Nullable String s) {
+            public void onPlaceSelected(@NonNull Place place) {
+
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
 
             }
         });
+
+        createViewModel.getAvailableSports().observe(this, new Observer<List<String>>() {
+            @Override
+            public void onChanged(List<String> strings) {
+                if(strings != null){
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
+                            android.R.layout.simple_spinner_item, strings.toArray(new String[0]));
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sportSpinner.setAdapter(adapter);
+                }
+            }
+        });
+
+        createViewModel.getSelectedDate().observe(this, new Observer<Calendar>() {
+            @Override
+            public void onChanged(Calendar calendar) {
+                dateTextView.setText(dateformat.format(calendar.getTime()));
+            }
+        });
+
+        createViewModel.getSelectedStartTime().observe(this, new Observer<Calendar>() {
+            @Override
+            public void onChanged(Calendar calendar) {
+                startsAtTextView.setText(timeformat.format(calendar.getTime()));
+            }
+        });
+
+        createViewModel.getSelectedEndTime().observe(this, new Observer<Calendar>() {
+            @Override
+            public void onChanged(Calendar calendar) {
+                endsAtTextView.setText(timeformat.format(calendar.getTime()));
+            }
+        });
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment datePicker = new DatePickerFragment(createViewModel.getSelectedDate().getValue(), Calendar.getInstance(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(i, i1, i2);
+
+                        createViewModel.getSelectedDate().setValue(calendar);
+                    }
+                });
+                datePicker.show(getFragmentManager(), "datepicker");
+            }
+        });
+
+        startsAt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker = new TimePickerFragment(createViewModel.getSelectedStartTime().getValue(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(Calendar.HOUR_OF_DAY, i);
+                        calendar.set(Calendar.MINUTE, i1);
+
+                        createViewModel.getSelectedStartTime().setValue(calendar);
+                    }
+                });
+                timePicker.show(getFragmentManager(), "timepicker");
+            }
+        });
+
+        endsAt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker = new TimePickerFragment(createViewModel.getSelectedEndTime().getValue(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(Calendar.HOUR_OF_DAY, i);
+                        calendar.set(Calendar.MINUTE, i1);
+
+                        createViewModel.getSelectedEndTime().setValue(calendar);
+                    }
+                });
+                timePicker.show(getFragmentManager(), "timepicker");
+            }
+        });
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().show();
+
         return root;
     }
 }
--- app/src/main/java/com/usf/pickup/ui/create/CreateViewModel.java
+++ app/src/main/java/com/usf/pickup/ui/create/CreateViewModel.java
@@ -1,19 +1,71 @@
 package com.usf.pickup.ui.create;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class CreateViewModel extends ViewModel {
+import com.usf.pickup.Pickup;
+import com.usf.pickup.api.ApiClient;
+import com.usf.pickup.api.ApiResult;
+import com.usf.pickup.api.models.User;
 
-    private MutableLiveData<String> mText;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
-    public CreateViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+public class CreateViewModel extends AndroidViewModel {
+
+    private MutableLiveData<List<String>> availableSports;
+    private MutableLiveData<Calendar> selectedDate = new MutableLiveData<>();
+    private MutableLiveData<Calendar> selectedStartTime = new MutableLiveData<>();
+    private MutableLiveData<Calendar> selectedEndTime = new MutableLiveData<>();
+
+    private void initializeUserData() {
+        availableSports = new MutableLiveData<>();
+        availableSports.setValue(new ArrayList<String>());
+
+        Pickup pickup = getApplication();
+
+        ApiClient.getInstance(pickup).getSports(pickup.getJwt(), new ApiResult.Listener<List<String>>() {
+            @Override
+            public void onResponse(ApiResult<List<String>> response) {
+                availableSports.setValue(response.getData());
+            }
+        });
+
+        selectedDate.setValue(Calendar.getInstance());
+        selectedStartTime.setValue(Calendar.getInstance());
+
+        // Default end time of 1 hour after now
+        Calendar end = Calendar.getInstance();
+        end.add(Calendar.HOUR_OF_DAY, 1);
+
+        selectedEndTime.setValue(end);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public CreateViewModel(Application application) {
+        super(application);
+        initializeUserData();
+    }
+
+    public MutableLiveData<List<String>> getAvailableSports() {
+        return availableSports;
+    }
+
+    public MutableLiveData<Calendar> getSelectedDate() {
+        return selectedDate;
+    }
+
+    public MutableLiveData<Calendar> getSelectedStartTime() {
+        return selectedStartTime;
+    }
+
+    public MutableLiveData<Calendar> getSelectedEndTime() {
+        return selectedEndTime;
     }
 }
