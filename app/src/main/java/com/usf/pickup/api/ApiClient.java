b7d41709cb4c8dc226dbc98e301e9f75250cb87d
JT <jtcullen1998@gmail.com>
2019-10-30 22:57:42 -0400

    Create page working

--- app/src/main/java/com/usf/pickup/api/ApiClient.java
+++ app/src/main/java/com/usf/pickup/api/ApiClient.java
@@ -11,15 +11,18 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
 import com.usf.pickup.R;
+import com.usf.pickup.api.models.Game;
 import com.usf.pickup.api.models.User;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -212,5 +215,52 @@ public class ApiClient {
         };
 
         addToRequestQueue(jsonArrayRequest);
+    }
+
+    public void createGame(final String jwt, final String title, final String sport, final int numberOfPlayers, final String address, final String locationName, final LatLng location, final String description, final Calendar startTime, final Calendar endTime, final ApiResult.Listener<Game> listener){
+        try {
+            String url = ctx.getResources().getString(R.string.api_url) +
+                    ctx.getResources().getString(R.string.game);
+
+            final JSONObject params = new JSONObject();
+            final JSONArray locationArray = new JSONArray();
+            locationArray.put(location.latitude);
+            locationArray.put(location.longitude);
+
+            params.put("title", title);
+            params.put("sport", sport);
+            params.put("numberOfPlayers", numberOfPlayers);
+            params.put("address", address);
+            params.put("locationName", locationName);
+            params.put("location",  locationArray);
+            params.put("description", description);
+            params.put("startTime", startTime.getTimeInMillis());
+            params.put("endTime", endTime.getTimeInMillis());
+
+            GsonRequest<Game> gsonRequest = new GsonRequest<>(Request.Method.POST, url, Game.class, params, Collections.singletonMap("Authorization", "Bearer " + jwt), new Response.Listener<Game>() {
+                @Override
+                public void onResponse(Game response) {
+                    listener.onResponse(ApiResult.Success(response));
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    if(error.networkResponse != null && error.networkResponse.data != null){
+                        try {
+                            JSONObject response = new JSONObject(new String(error.networkResponse.data, "UTF-8"));
+
+                            listener.onResponse(ApiResult.<Game>Error(response.getString("message")));
+                            return;
+                        } catch (JSONException | UnsupportedEncodingException ignored) {
+                        }
+                    }
+
+                    listener.onResponse(ApiResult.<Game>Error(ctx.getString(R.string.create_failed)));
+                }
+            });
+
+            addToRequestQueue(gsonRequest);
+        } catch (JSONException ignored) {
+        }
     }
 }
--- app/src/main/java/com/usf/pickup/ui/create/CreateViewModel.java
+++ app/src/main/java/com/usf/pickup/ui/create/CreateViewModel.java
@@ -7,9 +7,13 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.model.Place;
 import com.usf.pickup.Pickup;
+import com.usf.pickup.R;
 import com.usf.pickup.api.ApiClient;
 import com.usf.pickup.api.ApiResult;
+import com.usf.pickup.api.models.Game;
 import com.usf.pickup.api.models.User;
 
 import java.util.ArrayList;
@@ -19,14 +23,39 @@ import java.util.Collections;
 import java.util.List;
 
 public class CreateViewModel extends AndroidViewModel {
-
-    private MutableLiveData<List<String>> availableSports;
+    private MutableLiveData<List<String>> availableSports = new MutableLiveData<>();
     private MutableLiveData<Calendar> selectedDate = new MutableLiveData<>();
     private MutableLiveData<Calendar> selectedStartTime = new MutableLiveData<>();
     private MutableLiveData<Calendar> selectedEndTime = new MutableLiveData<>();
+    private MutableLiveData<Place> place = new MutableLiveData<>();
+    private MutableLiveData<CreateFormState> createFormState = new MutableLiveData<>();
+    private MutableLiveData<ApiResult<Game>> createResult = new MutableLiveData<>();
+
+    public void createGame(final String title, final String sport, final int numberOfPlayers, final Place place, final String description, final Calendar selectedDate, final Calendar selectedStartTime, final Calendar selectedEndTime){
+        Pickup pickup = getApplication();
+
+        Calendar startTime = (Calendar) selectedDate.clone();
+        Calendar endTime = (Calendar) selectedDate.clone();
+
+        startTime.set(Calendar.HOUR_OF_DAY, selectedStartTime.get(Calendar.HOUR_OF_DAY));
+        startTime.set(Calendar.MINUTE, selectedStartTime.get(Calendar.MINUTE));
+        startTime.set(Calendar.SECOND, selectedStartTime.get(Calendar.SECOND));
+        startTime.set(Calendar.MILLISECOND, selectedStartTime.get(Calendar.MILLISECOND));
+
+        endTime.set(Calendar.HOUR_OF_DAY, selectedEndTime.get(Calendar.HOUR_OF_DAY));
+        endTime.set(Calendar.MINUTE, selectedEndTime.get(Calendar.MINUTE));
+        endTime.set(Calendar.SECOND, selectedEndTime.get(Calendar.SECOND));
+        endTime.set(Calendar.MILLISECOND, selectedEndTime.get(Calendar.MILLISECOND));
+
+        ApiClient.getInstance(pickup).createGame(pickup.getJwt(), title, sport, numberOfPlayers, place.getAddress(), place.getName(), place.getLatLng(), description, startTime, endTime, new ApiResult.Listener<Game>() {
+            @Override
+            public void onResponse(ApiResult<Game> response) {
+                createResult.setValue(response);
+            }
+        });
+    }
 
     private void initializeUserData() {
-        availableSports = new MutableLiveData<>();
         availableSports.setValue(new ArrayList<String>());
 
         Pickup pickup = getApplication();
@@ -53,6 +82,57 @@ public class CreateViewModel extends AndroidViewModel {
         initializeUserData();
     }
 
+    public void createDataChanged(final String title, Object sport, final String players, final String description, final Place place){
+        CreateFormState create = new CreateFormState();
+
+        if (!isTitleValid(title)) {
+            create.setTitleError(R.string.invalid_game_title);
+        }
+
+        if (!isSportValid(sport)) {
+            create.setSportsError(R.string.invalid_game_sport);
+        }
+
+        if (!isTotalPlayersValid(players)) {
+            create.setPlayersError(R.string.invalid_game_players);
+        }
+
+        if (!isDescriptionValid(description)) {
+            create.setDescriptionError(R.string.invalid_game_description);
+        }
+
+        if (!isPlaceValid(place)) {
+            create.setLocationError(R.string.invalid_game_location);
+        }
+
+        createFormState.setValue(create);
+    }
+
+    private boolean isTitleValid(String title) {
+        return title != null && title.length() >= 1 && title.length() <= 140;
+    }
+
+    private boolean isSportValid(Object sport) {
+        return sport != null;
+    }
+
+    private boolean isPlaceValid(Place place) {
+        return place != null;
+    }
+
+    private boolean isTotalPlayersValid(String players) {
+        try {
+            int playersInt = Integer.parseInt(players);
+            return playersInt >= 2 && playersInt <= 250;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isDescriptionValid(String description) {
+        return description != null && description.length() <= 258;
+    }
+
     public MutableLiveData<List<String>> getAvailableSports() {
         return availableSports;
     }
@@ -67,5 +147,17 @@ public class CreateViewModel extends AndroidViewModel {
 
     public MutableLiveData<Calendar> getSelectedEndTime() {
         return selectedEndTime;
+    }
+
+    public MutableLiveData<CreateFormState> getCreateFormState() {
+        return createFormState;
+    }
+
+    public MutableLiveData<Place> getPlace() {
+        return place;
+    }
+
+    public MutableLiveData<ApiResult<Game>> getCreateResult() {
+        return createResult;
     }
 }
